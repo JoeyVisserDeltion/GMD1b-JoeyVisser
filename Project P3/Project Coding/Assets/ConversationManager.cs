084d74693b58cde4ec12c822d8718a6071bbc6e5
Joey Visser <HipsterJCV@gmail.com>
2017-04-14 17:51:54 +0200

    Project Coding Added
    Added the coding files for Development.

---
+++ Project P3/Project Coding/Assets/ConversationManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversationManager : MonoBehaviour {
+
+    // Options variables
+    public bool optionOne;
+    public bool optionTwo;
+
+    // Custom text variables
+    public string one;
+    public string two;
+
+    public string mainText;
+
+    // Trigger conversation
+    void OnCollisionEnter()
+    {
+        print(mainText);
+    }
+
+    // Choose options via bool
+    void Update()
+    {
+        if(optionOne == true)
+        {
+            print(one);
+            optionOne = false;
+        }
+
+        if(optionTwo == true)
+        {
+            print(two);
+            optionTwo = false;
+        }
+    }
+}
---
+++ Project P3/Project Coding/Assets/PickUpItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpItem : MonoBehaviour {
+
+    // Item variable
+    public string item;
+
+    // Pick up item
+    void OnCollisionEnter()
+    {
+        print(item);
+        Destroy(gameObject);
+    }
+}
