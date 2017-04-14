ac53404134822b92399b011a261306003b4b50fd
Joey Visser <HipsterJCV@gmail.com>
2017-04-14 23:13:17 +0200

    Item Manager Added
    Added a way to pick up items and store them in an item list.

---
+++ Project P3/Project Coding/Assets/GameManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    // Item list
+    public List<string> items = new List<string>();
+}
