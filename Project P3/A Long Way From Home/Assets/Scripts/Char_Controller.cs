e85c34d49f4a9ab6052e4cc7ab8052b67927cf73
Joey Visser <HipsterJCV@gmail.com>
2017-04-21 13:07:56 +0200

    Project P3 Added
    First hand-in of Project P3

---
+++ Project P3/A Long Way From Home/Assets/Scripts/Char_Controller.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Char_Controller : MonoBehaviour {
+
+    // Variables for movement
+    public Vector3 movement;
+    public float hor;
+    public float ver;
+    public float speed;
+
+    // Variables for rotation
+    public Vector3 rotation;
+    public Vector3 camRotation;
+    public GameObject cam;
+    public float camSpeed;
+
+    // Movement + Rotation
+    void FixedUpdate()
+    {
+        // Movement
+        hor = Input.GetAxis("Horizontal");
+        ver = Input.GetAxis("Vertical");
+
+        movement.x = hor;
+        movement.y = -ver;
+
+        transform.Translate(movement * speed * Time.deltaTime);
+
+        // Rotation
+        rotation.z = Input.GetAxis("Mouse X");
+        camRotation.x = Input.GetAxis("Mouse Y");
+
+        transform.Rotate(rotation * camSpeed * Time.deltaTime);
+        transform.Rotate(-camRotation * camSpeed * Time.deltaTime);
+    }
+}
