9faa8d3affabb9c1c6164bb701badfdec68d646b
Joey Visser <HipsterJCV@gmail.com>
2017-06-23 22:14:05 +0200

    Project 4 Inventory System
    Voor Frans: Classes staan in de "Scripts" folder in de Assets

---
+++ Inventory System/Assets/Scripts/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour {
+
+    public Sprite itemImage;
+    public string itemName;
+    public string itemDescription;
+    public int itemValue;
+}
---
+++ Inventory System/Assets/Scripts/ItemSlot.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSlot : MonoBehaviour {
+
+    public Item item;
+    public Item itemHolder;
+    public Image image;
+    public Inventory inventory;
+
+    void Awake()
+    {
+        inventory = GameObject.Find("Inventory").GetComponent<Inventory>();
+    }
+
+    void Update()
+    {
+        if(item != null)
+        {
+            image.sprite = item.itemImage;
+            image.enabled = true;
+        }
+    }
+
+    void OnClick()
+    {
+        if(item == null)
+        {
+            if(inventory.activeItem != null)
+            {
+                item = inventory.activeItem;
+                inventory.activeItem = null;
+                image.sprite = item.itemImage;
+                image.enabled = true;
+            }
+        }
+        else
+        {
+            if(inventory.activeItem == null)
+            {
+                inventory.activeItem = item;
+                item = null;
+                image.sprite = null;
+                image.enabled = false;
+            }
+            else
+            {
+                itemHolder = item;
+                item = inventory.activeItem;
+                inventory.activeItem = itemHolder;
+                itemHolder = null;
+                image.sprite = item.itemImage;
+                image.enabled = true;
+            }
+        }
+    }
+}
